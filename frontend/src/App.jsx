8e2b0ad8bc97c98d5f8a978c71d3d7959784e716
Premium07 <premsagarg23@gmail.com>
2025-01-23 14:43:06 +0545

    add routing and navigation with Navbar and pages for Home, Sign Up, Login, Settings, and Profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
 const App = () => {
-  return <div className="text-4xl">App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/settings" element={<SettingsPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
