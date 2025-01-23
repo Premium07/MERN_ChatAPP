7e36742aa0eacb4a8e07bdec6b4acff8336595b3
Premium07 <premsagarg23@gmail.com>
2025-01-23 11:47:22 +0545

    initialize backend server with Express, dotenv, and essential middleware

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
