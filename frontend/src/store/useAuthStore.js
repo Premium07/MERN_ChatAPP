f85c721dc40b3df72618f6a118837aefa53e8ed8
Premium07 <premsagarg23@gmail.com>
2025-01-24 21:33:27 +0545

    feat: implement login page with form handling, password visibility toggle, and loading state

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -14,8 +14,8 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data, isCheckingAuth: false });
     } catch (error) {
-      console.log(error);
       set({ authUser: null });
+      toast.error(error.response.data.message);
     } finally {
       set({ isCheckingAuth: false });
     }
@@ -33,4 +33,27 @@ export const useAuthStore = create((set) => ({
       set({ isSigningUp: false });
     }
   },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success(res.data.message);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  }
 }));
