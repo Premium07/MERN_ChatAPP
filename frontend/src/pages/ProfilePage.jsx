205c5689f8e9b043af72a7a3023aa835493e7b10
Premium07 <premsagarg23@gmail.com>
2025-01-26 11:58:14 +0545

    feat: implement theme selection and storage functionality with theme preview in SettingsPage

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -21,7 +21,7 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className="h-screen pt-15">
+    <div className="min-h-screen pt-15">
       <div className="max-w-2xl mx-auto p-4">
         <div className="bg-base-300 rounded-xl p-2 space-y-8">
           <div className="text-center">
