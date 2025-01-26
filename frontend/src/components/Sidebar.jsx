a127ec8ccb4f7ed00fdf8c9c38e84419b9445012
Premium07 <premsagarg23@gmail.com>
2025-01-26 16:00:14 +0545

    feat: implement ChatContainer, ChatHeader, and MessageInput components; add message handling and loading states

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
     <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
       <div className="border-b border-base-300 w-full p-4">
         <div className="flex items-center gap-2">
-          <Users className="size-5" />
+          <Users className="size-6" />
           <span className="font-medium hidden lg:block">Contacts</span>
         </div>
         {/* online filter toggle */}
@@ -31,7 +31,7 @@ const Sidebar = () => {
               onClick={() => setSelectedUser(user)}
               className={`
               w-full cursor-pointer mt-2 p-2 flex items-center gap-3
-              hover:bg-base-300 rounded-xl transition-colors
+              hover:bg-base-300 rounded transition-colors
               ${
                 selectedUser?._id === user._id
                   ? "bg-base-300 ring-1 ring-base-300"
